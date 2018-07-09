0720e34bc2c471d8f035a4983c380be2431a8d48
aerison <sonsoo1224@gmail.com>
2018-07-09 07:03:51 +0000

    image upload tinymce

--- app/controllers/tinymce_assets_controller.rb
+++ app/controllers/tinymce_assets_controller.rb
@@ -16,9 +16,14 @@ class TinymceAssetsController < ApplicationController
 
   private
   def upload_image(file)
-      s3=Aws::S3::Resource/new(region: ENV['AWS_REGION'])
-      obj=s3.bucket(ENV["S3S3_BUCKET_NAME"]).object('/posts/content/'+filename(file))
+      s3=Aws::S3::Resource.new(region: ENV['AWS_REGION'])
+      obj=s3.bucket(ENV["S3_BUCKET_NAME"]).object('posts/content/'+filename(file))
       obj.upload_file(file.tempfile, {acl:'public-read'})
       obj.public_url.to_s
+  end
+
+
+  def filename(file)
+      "#{Time.now.to_i}_#{file.original_filename}"
   end
 end
